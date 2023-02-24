3a5966651a2dfd50988f895fc2a8a543de050b52
guille orradre <jokareversi@gmail.com>
2023-02-23 21:53:11 -0300

    fixing category routing

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,68 +1,62 @@
-import Container from "react-bootstrap/Container";
 import logoimg from "../../imgs/tori-icon.png";
-import './Navbar.css'
+import "./Navbar.css";
 import { Link } from "react-router-dom";
-
-// IMPORT PAGES
-import Home from "../../Pages/Home/Home";
-import Contacto from "../../Pages/Contacto/Contacto";
-import FigurasAnime from "../../Pages/FigurasAnime/FigurasAnime";
-import FigurasComic from "../../Pages/FigurasComic/FigurasComic";
-import Otros from "../../Pages/Otros/Otros";
-
 
 // IMPORT COMPONENTS
 import CartWidget from "../CartWidget/CartWidget";
 
 const Navbar = () => {
   return (
-    <nav className="Navigation">
-      <div className="divLogo">
-      <img src={logoimg} className="logoTori" alt="logo" />
-      <p className="textBrand">ToriShop</p>
+    <nav className="Navigation navbar navbar-expand-lg">
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <img src={logoimg} className="logoTori" alt="logo" />
+        <p className="textBrand">ToriShop</p>
+
+        <ul className="List-ul navbar-nav mr-auto">
+          <Link to="/" className="Link">
+            Home
+          </Link>
+          <Link to="/Contacto" className="Link">
+            Contacto
+          </Link>
+          <li class="nav-item dropdown">
+            
+            {/* eslint-disable */}
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              Categorias
+            </a>
+            
+            {/* eslint-enable */}
+
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <Link to="/FigurasAnime" className="dropdown-item">
+                Figuras Anime
+              </Link>
+              <Link to="/FigurasComic" className="dropdown-item">
+                Figuras Comic
+              </Link>
+              <div class="dropdown-divider"></div>
+              <Link to="/Otros" className="dropdown-item">
+                Otros
+              </Link>
+            </div>
+          </li>
+        </ul>
+        <div className="divCart">
+          <CartWidget />
+        </div>
       </div>
-      
-      <ul className="List-ul">
-        
-      <Link to="/" className="Link">Home</Link>
-      <Link to="/Contacto" className="Link">Contacto</Link>
-      <Link to="/FigurasAnime" className="Link">Figuras Anime</Link>
-      <Link to="/FigurasComic" className="Link">Figuras Comic</Link>
-      <Link to="/Otros" className="Link">Otros</Link> 
-      </ul>
-      <div className="divCart">
-      <CartWidget/>
-      </div>
-     
     </nav>
-    
   );
-}
+};
 
 export default Navbar;
 
-// <Navbar bg="light" expand="lg">
-//       <Container className="contNav">
-//         <div className="divBrand" >
-//           <img src={logoimg} className="logoTori" alt="logo" />
-//           <p href="#home" className="textBrand">ToriShop2</p>
-//         </div>
-
-//         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//         <Navbar.Collapse id="basic-navbar-nav">
-//           <Nav className="me-auto">
-//             <Nav.Link className="categoriesNav" href="#home">Home</Nav.Link>
-//             <Nav.Link className="categoriesNav" href="#link">Contacto</Nav.Link>
-//             <NavDropdown className="categoriesNav" title="Productos" id="basic-nav-dropdown">
-//               <NavDropdown.Item href="#action/3.1">Figuras Anime</NavDropdown.Item>
-//               <NavDropdown.Item href="#action/3.2">
-//                 Figuras Comics
-//               </NavDropdown.Item>
-//               <NavDropdown.Item href="#action/3.3">Otros</NavDropdown.Item>
-//               <NavDropdown.Divider />
-//             </NavDropdown>
-//           </Nav>
-//         </Navbar.Collapse>
-//       </Container>
-//       <CartWidget/>
-//     </Navbar>
